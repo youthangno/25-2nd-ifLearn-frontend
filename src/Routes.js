19463d36a8d866e10fb3ba4bda97dc16c3f478a6
이상철 <zlrz6706@gmail.com>
2021-10-18 20:36:59 +0900

    초기설정 오타수정입니다.

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Login from './pages/Login/Login';
-import SignUp from './pages/Signup/Signup';
+import Signup from './pages/Signup/Signup';
 import Main from './pages/Main/Main';
 import Video from './pages/Video/Video';
 import Courses from './pages/Courses/Courses';
@@ -15,7 +15,7 @@ class Routes extends React.Component {
         <Nav />
         <Switch>
           <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/signup" component={Signup} />
           <Route exact path="/" component={Main} />
           <Route exact path="/video" component={Video} />
           <Route exact path="/course" component={Course} /> {/* 상세페이지 */}
