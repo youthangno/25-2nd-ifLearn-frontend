8a3de5d4496dfaa5a3a7e937151b484b10db77d2
이상철 <zlrz6706@gmail.com>
2021-10-23 17:51:07 +0900

    Modify: 멘토님 요구사항 수정완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import GlobalStyle from './styles/GlobalStyle';
 
---
+++ src/pages/Main/MainSlider.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Slider from 'react-slick';
+import styled from 'styled-components';
+
+export default function MainSlider({ images }) {
+  return (
+    <Container>
+      <StyledSlider {...settings}>
+        {images.map((image, idx) => {
+          return (
+            <div key={idx}>
+              <Img src={image.image_url} alt={image.image_url} />
+            </div>
+          );
+        })}
+      </StyledSlider>
+    </Container>
+  );
+}
+const settings = {
+  infinite: true,
+  arrows: true,
+  prevArrow: null,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  pauseOnHover: true,
+
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+const Container = styled.div`
+  width: 100%;
+`;
+const StyledSlider = styled(Slider)`
+  width: 100%;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+`;
